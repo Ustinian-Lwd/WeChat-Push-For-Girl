3b0b6ea8454cab96c2e22534b8dc392f659ba986
Ustinian-Lwd <42571011+Ustinian-Lwd@users.noreply.github.com>
2022-12-04 12:40:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Rz3eBs9efrhD-5EDIrIH1HCYlYW9VY0Hpin3PHK0DYw',
 
   CALLBACK_USERS: [
     {
