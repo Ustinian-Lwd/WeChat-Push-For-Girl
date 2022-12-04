451f56ba4223e42629449c3770806ee0a54390a5
Ustinian-Lwd <42571011+Ustinian-Lwd@users.noreply.github.com>
2022-12-04 15:56:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -4,32 +4,16 @@ const USER_CONFIG = {
    * 基本配置
    */
   
-  // 使用的推送通道：['push-deer', 'wechat-test', 'server-chan', 'push-plus']
-  // 默认使用 【微信测试号】
-  // 使用【pushDeer】请填写 push-deer
-  // 使用【微信测试号】请填写 wechat-test
-  // 使用【方糖服务号】请填写 server-chan
-  // 使用【pushplus推送加服务号】请填写 push-plus
   USE_PASSAGE: 'wechat-test',
   
-  // 使用微信测试号时才需要填写：公众号APP_ID
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_ID: 'wx3c2f4f31f193246f',
 
-  // 使用微信测试号时才需要填写：公众号APP_SECRET
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: 'd220a5d56f049aa6ba5852eaf718f85b',
   
-  // 为了避免推送服务器误将脚本列为恶意推送脚本，可设置每分钟脚本最大推送数
-  // 每分钟脚本最大推送数，超过此数将会休眠1分钟后再发送剩余消息，不填则默认为5
-  // 此项不建议随意修改
   MAX_PUSH_ONE_MINUTE: 5,
-  // 配合MAX_PUSH_ONE_MINUTE使用，休眠<SLEEP_TIME>毫秒后再发送剩余消息，不填则默认为65000
+
   SLEEP_TIME: 65000,
 
-  // 功能开关,打开：true，关闭：false
   SWITCH: {
     /** 每日天气 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
@@ -44,19 +28,19 @@ const USER_CONFIG = {
     CIBA: true,
     
     // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    oneTalk: false,
+    oneTalk: true,
     
     // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     earthyLoveWords: true,
     
     // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    momentCopyrighting: false,
+    momentCopyrighting: true,
     
     // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    poisonChickenSoup: false,
+    poisonChickenSoup: true,
     
     // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    poetry: false,
+    poetry: true,
 
     /** 星座运势 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
@@ -94,12 +78,12 @@ const USER_CONFIG = {
   
   /** 是否给文字设置多彩颜色, 和emoji不兼容 */
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: true,
+  IS_SHOW_COLOR: false,
   
   /** 每日一言 */
   // 每日一言的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: '',
+  LITERARY_PREFERENCE: "",
   
 
   /**
@@ -107,32 +91,24 @@ const USER_CONFIG = {
    */
   USERS: [
     {
-      // 想要发送的人的名字
+
       name: '宝宝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'o0X4q6yOdSYIT1lRf9dg3WoiGryc',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: '_xURW7fJXNT9fc89q8foIPSayWnteuZaGzYHCJOnEn8',
-      // 所在省份或城市，也可以不填
       province: '江西',
-      // 所在城市或县区
       city: '赣州',
+	  
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '10-08',
+	  
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
+	  
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: 'https://weibo.com/hot/search',
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+	  
+      // 专属节日提醒
       festivals: [
-        
-		// 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          // type: '*生日', name: '宝宝', year: '1998', date: '10-08', 
-        },
-
         
 		// 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
@@ -144,115 +120,33 @@ const USER_CONFIG = {
       ],
 	  
 	  
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      // 专属纪念日/倒数日
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-03-05' },
       ],
 	  
       // 课程表相关配置
-      // 如果courseSchedule不存在或者为空（null）则认为没有课程
-      // 如果courseSchedule是一个数组，则认为不区分单双周，直接填写星期几对应的课表数据即可
-      // 如果courseSchedule是一个对象（如下面所示）
       courseSchedule: null
-    }, 
-
-    {
-      name: '宝宝01',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: '',
-      festivals: [],
-      customizedDateList: [],
-      courseSchedule: null
+	  
     }
-	
-    // 你可以不断按格式往下增加
 	
   ],
 
   /**
    * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
    */
-
-  // 使用微信测试号：【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  // 使用其他通道时，请填写 config/template-config.cjs 中【推送完成提醒】模板的id
   CALLBACK_TEMPLATE_ID: '05eiwLAre34w9HCH0ju6g403_dG3wzQOeO47wQy3ZGw',
 
-  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
     {
       // 一般都填自己
       name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'o0X4q65g0MwbYmX3AX9U5SO8t9VE',
     }
 
   ],
 
-  /**
-   * 信息配置说明
-   */
-
-  /** 
-   * 天气相关
-   * 
-   * 可以填 省份或者市区， PROVINCE 的区域必须包含 CITY
-   * 例如： PROVINCE：'广东' CITY：'惠州'
-   * 例如： PROVINCE：'咸阳' CITY：'长武'
-   *
-   * 甚至 PROVINCE 可以不填
-   * 例如： PROVINCE：'' CITY：'长武'
-   * */
-  
-  // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '咸阳',
-  // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '长武',
-
-  /** 重要节日相关 */
-
-  /**
-   * 重要节日，修改名字为对应需要显示的名字, date 仅填月日即可, 请严格按照示例填写
-   * type必须填！ 只能 “生日” 和 “节日” 二选一!
-   *
-   * --- 阴历配置开始 ---
-   * 如果日期使用阴历，请在 “生日” 或 "节日“ 前添加 * 符号
-   * --- 阴历配置结束 ---
-   *
-   * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
-   * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
-   *
-   * --- 是否展示周岁信息开始 ---
-   * isShowAge: true 展示岁数, 仅type为生日生效
-   * isShowAge: false 不展示岁数, 仅type为生日生效
-   * 删除isShowAge属性，也会不展示岁数
-   * --- 是否展示周岁信息结束 ---
-   */
-
-
-  /**
-   * 限制重要节日的展示条目, 需要填写数字;
-   * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
-   * 如果为0, 则默认展示全部
-   */
-
-  
-  // 默认的课表配置
-  COURSE_SCHEDULE: null,
-
-  /** 插槽 */
-
-  /** 你可以在这里写超多的你想显示的内容了！
-   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-   * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
-   * */
-  SLOT_LIST: null,
   
 }
 
